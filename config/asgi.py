48c26223aea3049e3880eba2d40e51ca631b2422
Bekzod <bekzod@example.com>
2026-03-11 06:55:57 +0500

    finish_new

--- config/asgi.py
+++ config/asgi.py
@@ -1,14 +1,17 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# get_asgi_application() ni middleware'lar importidan oldin chaqirish shart (AppRegistryNotReady xatoligi uchun)
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from api.middleware import JWTAuthMiddleware
 import api.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": JWTAuthMiddleware(
         AuthMiddlewareStack(
             URLRouter(
